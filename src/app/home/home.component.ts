14ae834fac92b77815831e1cca4caf6c1e24a81b
Pwsjas <pwsjas433@hotmail.com>
2023-01-05 06:49:45 -0500

    add socials, format basic layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { faEnvelope, faFile } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 
 @Component({
   selector: 'app-home',
@@ -6,6 +9,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  faEnvelope = faEnvelope;
+  faGithub = faGithub;
+  faFile = faFile;
 
   constructor() { }
 
