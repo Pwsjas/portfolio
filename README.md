8f44b47ba0a6199a4d2cc53634481d29cbded815
Pwsjas <pwsjas433@hotmail.com>
2023-01-11 12:29:02 -0500

    update README with images, cleanup variable default data

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Portfolio is exactly what it sounds like, my personal web-portfolio intended to show off my best web-based projects.
 It is available at `https://philipsimpson.ca`, or by running the application here, using the Angular cli's `ng serve` to generate a development server available at `http://localhost:4200/`.
 
-<!-- ![image of the entire website](https://github.com/Pwsjas/wowtracker/blob/main/app/docs/site.png?raw=true) -->
+![image of the entire website](https://raw.githubusercontent.com/Pwsjas/portfolio/main/docs/portfolio.png)
--- src/app/home/main-content/description/description.component.ts
+++ src/app/home/main-content/description/description.component.ts
@@ -9,7 +9,7 @@ export class DescriptionComponent implements OnInit {
 
   @Input() title: string = '';
   @Input() images: string[] = [];
-  @Input() description: string = 'My name is Philip, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac rhoncus tortor. Integer dictum eros ut neque tincidunt, eget ullamcorper lectus laoreet. Donec malesuada elit sit amet commodo suscipit. Nam lobortis pretium risus, ut euismod est lacinia non. Nulla pellentesque velit vel condimentum pellentesque. Sed.';
+  @Input() description: string = '';
   @Input() links: string[] = [];
 
 
